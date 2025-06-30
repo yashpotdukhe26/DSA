02f2ae758350abcd49df8ca329220da2d3121193
yashpotdukhe26 <161602802+yashpotdukhe26@users.noreply.github.com>
2025-06-30 09:23:16 +0530

    Add files via upload

---
+++ mergesort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+mergesort(arr[], int left,int right){
+    int n1 =mid-left+1;
+    int n2=right-mid;
+
+    int a[n1] ,int b[n2];
+    for(int i=0; i<n1; i++)
+
+    if(left<right){
+        int mid=(left+right)/2;
+        mergesort(arr,1,mid);
+        mergesort(arr,mid+1,r);
+    }
+}
+int main(){
+    int n;
+    int arr[]={1,2,3,4,5,6,8};
+    int n=size(arr)/sizeof(arr[0]){
+
+    }
+
+
+
+    return 0;
+}
